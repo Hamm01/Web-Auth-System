5636d1e44c4699d25b44ec9f16c85f9db7d4992a
Himanish kochhar <himanish.kochhar@gmail.com>
2025-01-26 02:07:12 +0530

    shadcn ui components file added installed

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="flex justify-center items-center min-h-screen min-w-screen">
+      <span className="font-black text-4xl ">Auth web system.</span>
+    </div>
+  );
+}
