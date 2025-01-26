89bb021f85be7c8b4b4b84039c1f9b35c231c392
Himanish kochhar <himanish.kochhar@gmail.com>
2025-01-27 05:21:03 +0530

    Added new button for signin and sign up

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,17 @@
+import { Button } from "@/components/ui/button";
+
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="flex justify-center items-center min-h-screen min-w-screen">
-      <span className="font-black text-4xl ">Auth web system.</span>
+    <div className="my-6 px-4 max-w-md mx-auto">
+      <div className="text-center space-y-6">
+        <h1 className="text-3xl font-bold"> Auth web system</h1>
+        <Button asChild size="lg" >
+          <Link href="/auth/login"> Sign In / Sign Up</Link>
+        </Button>
+      </div>
     </div>
   );
 }
