a295ae350841531bd38fdafdc097815d80ae84f3
Himanish kochhar <himanish.kochhar@gmail.com>
2025-02-02 04:23:25 +0530

    added the tabs content for the signup and signin tabs

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -1,3 +1,4 @@
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 
 export default function LoginPage() {
@@ -6,5 +7,23 @@ export default function LoginPage() {
             <TabsTrigger value="signin">Sign In</TabsTrigger>
             <TabsTrigger value="signup">Sign Up</TabsTrigger>
         </TabsList>
+        <Card>
+            <TabsContent value="signin">
+                <CardHeader className="text-2xl font-bold">
+                    <CardTitle>Sign IN</CardTitle>
+                </CardHeader>
+                <CardContent>
+                    <p>Sign in form goes here</p>
+                </CardContent>
+            </TabsContent>
+            <TabsContent value="signup">
+                <CardHeader className="text-2xl font-bold">
+                    <CardTitle>Sign Up</CardTitle>
+                </CardHeader>
+                <CardContent>
+                    <p>Sign up form goes here</p>
+                </CardContent>
+            </TabsContent>
+        </Card>
     </Tabs>
 }
