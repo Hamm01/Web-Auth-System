124648e603911a9b3eee1b659f8d866a8401b9a5
Himanish kochhar <himanish.kochhar@gmail.com>
2025-02-03 13:19:47 +0530

    added the react-hook-form, using shadcn, this boilerplate code for signup form

--- src/app/auth/login/_components/sign-up-tab.tsx
+++ src/app/auth/login/_components/sign-up-tab.tsx
@@ -1,4 +1,8 @@
+"use client"
+import { useForm } from "react-hook-form"
+import { zodResolver } from '@hookform/resolvers/zod'
 import z from "zod"
+import { Form } from "@/components/ui/form"
 
 const signUpSchema = z.object({
     name: z.string().min(1),
@@ -6,6 +10,25 @@ const signUpSchema = z.object({
     password: z.string().min(6),
 })
 
+type SignUpForm = z.infer<typeof signUpSchema>
+
 export function SignUpTab() {
-    return null
+    const form = useForm<SignUpForm>({
+        resolver: zodResolver(signUpSchema),
+        defaultValues: {
+            name: "",
+            email: "",
+            password: "",
+        },
+    })
+    function handleSignUp(data: SignUpForm) {
+        console.log(data)
+    }
+
+    return <Form {...form}>
+        <form className="space-y-4" onSubmit={form.handleSubmit(handleSignUp)}>
+
+        </form>
+
+    </Form>
 }
