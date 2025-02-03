77e4dc508856ff171c96a1f308ed04767efec9e9
Himanish kochhar <himanish.kochhar@gmail.com>
2025-02-03 11:23:28 +0530

    added the Sign up schema using zod, in signup tab component

--- src/app/auth/login/_components/sign-up-tab.tsx
+++ src/app/auth/login/_components/sign-up-tab.tsx
@@ -1,3 +1,11 @@
+import z from "zod"
+
+const signUpSchema = z.object({
+    name: z.string().min(1),
+    email: z.email().min(1),
+    password: z.string().min(6),
+})
+
 export function SignUpTab() {
     return null
 }
