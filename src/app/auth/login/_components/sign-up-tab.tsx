33e9080742528aabac0309306c56e288ed077219
Himanish kochhar <himanish.kochhar@gmail.com>
2025-02-04 05:56:27 +0530

    Added the Signup form with fiellds and button to submit

--- src/app/auth/login/_components/sign-up-tab.tsx
+++ src/app/auth/login/_components/sign-up-tab.tsx
@@ -2,7 +2,11 @@
 import { useForm } from "react-hook-form"
 import { zodResolver } from '@hookform/resolvers/zod'
 import z from "zod"
-import { Form } from "@/components/ui/form"
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
+import { PasswordInput } from "@/components/ui/password-input"
+import { Button } from "@/components/ui/button"
+import { LoadingSwap } from "@/components/ui/loading-swap"
 
 const signUpSchema = z.object({
     name: z.string().min(1),
@@ -21,13 +25,46 @@ export function SignUpTab() {
             password: "",
         },
     })
+    const { isSubmitting } = form.formState
     function handleSignUp(data: SignUpForm) {
         console.log(data)
     }
 
     return <Form {...form}>
         <form className="space-y-4" onSubmit={form.handleSubmit(handleSignUp)}>
+            <FormField control={form.control} name="name" render={({ field }) => (
+                <FormItem>
+                    <FormLabel>Name</FormLabel>
+                    <FormControl>
+                        <Input {...field} />
+                    </FormControl>
+                    <FormMessage />
+                </FormItem>
+            )} />
+            <FormField control={form.control} name="email" render={({ field }) => (
+                <FormItem>
+                    <FormLabel>Email</FormLabel>
+                    <FormControl>
+                        <Input type="email" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                </FormItem>
+            )} />
+            <FormField control={form.control} name="password" render={({ field }) => (
+                <FormItem>
+                    <FormLabel>Password</FormLabel>
+                    <FormControl>
+                        <PasswordInput  {...field} />
+                    </FormControl>
+                    <FormMessage />
+                </FormItem>
+            )} />
+            <Button type="submit" disabled={isSubmitting} className="w-full">
+                <LoadingSwap isLoading={isSubmitting}>
 
+                    SignUp
+                </LoadingSwap>
+            </Button>
         </form>
 
     </Form>
