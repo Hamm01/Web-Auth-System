384155ca098d7cbb42ba5c1e7c66c68fe10ff556
Himanish kochhar <himanish.kochhar@gmail.com>
2025-02-12 07:30:18 +0530

    replaced the button with betterauthaction button

--- src/app/auth/login/_components/social-auth-buttons.tsx
+++ src/app/auth/login/_components/social-auth-buttons.tsx
@@ -1,5 +1,6 @@
 "use client"
 
+import { BetterAuthActionButton } from '@/components/auth/better-auth-action-button'
 import { Button } from '@/components/ui/button'
 import { authClient } from '@/lib/auth-client'
 import { SUPPORT_OAUTH_PROVIDERS, SUPPORT_OAUTH_PROVIDERS_DETAILS } from '@/lib/o-auth-providers'
@@ -9,11 +10,13 @@ export function SocialAuthButtons() {
     return SUPPORT_OAUTH_PROVIDERS.map((provider) => {
         const Icon = SUPPORT_OAUTH_PROVIDERS_DETAILS[provider].Icon
 
-        return <Button variant="outline" key={provider} className='w-full' onClick={() => {
-            authClient.signIn.social({ provider, callbackURL: "/" })
-        }}>
-            <Icon />
-            {SUPPORT_OAUTH_PROVIDERS_DETAILS[provider].name}
-        </Button>
+        return (
+            <BetterAuthActionButton variant="outline" key={provider} action={() => {
+                return authClient.signIn.social({ provider, callbackURL: "/" })
+            }}>
+                <Icon />
+                {SUPPORT_OAUTH_PROVIDERS_DETAILS[provider].name}
+            </BetterAuthActionButton>
+        )
     })
 }
